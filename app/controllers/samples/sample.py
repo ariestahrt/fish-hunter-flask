3f8c2aae70bc7f72e9744eab21af5e45074e08c1
Ariesta <ikadekagusariestaputra@gmail.com>
2023-03-17 00:30:08 +0800

    Add get_samples

--- app/controllers/samples/sample.py
+++ app/controllers/samples/sample.py
@@ -6,12 +6,86 @@ import json
 
 from bson.objectid import ObjectId
 import requests
+import pymongo
 
 logger = current_app.config["LOGGER"]
 samples = Blueprint('samples', __name__, url_prefix='/api/v1/samples')
 DB = current_app.config["DB"]
 SAMPLES = DB["samples"]
 DATASETS = DB["datasets"]
+
+@samples.route("", methods=['GET'])
+def get_samples():
+    logger.info("Getting datasets")
+    # Pagination parameters
+    draw = int(request.args.get('draw'))
+    page = int(request.args.get('page'))
+    page_size = int(request.args.get('page_size'))
+
+    search = request.args.get('search[value]')
+    offset = (page - 1) * page_size
+
+    # Get the columns
+    col_idx = 0
+    while True:
+        if request.args.get(f'columns[{col_idx}][data]') == None:
+            break
+        col_idx += 1
+    
+    columns = []
+    for i in range(col_idx):
+        columns.append({
+            "data": request.args.get(f'columns[{i}][data]') if request.args.get(f'columns[{i}][data]') != "" else None,
+            "name": request.args.get(f'columns[{i}][name]'),
+            "searchable": True if request.args.get(f'columns[{i}][searchable]') == "true" else False,
+            "orderable": True if request.args.get(f'columns[{i}][orderable]') == "true" else False,
+            "search_value": request.args.get(f'columns[{i}][search][value]'),
+            "search_regex": True if request.args.get(f'columns[{i}][search][regex]') == "true" else False,
+        })
+
+    # Get the order
+    order_idx = 0
+    while True:
+        if request.args.get(f'order[{order_idx}][column]') == None:
+            break
+        order_idx += 1
+    
+    order = []
+    for i in range(order_idx):
+        order.append({
+            "column": int(request.args.get(f'order[{i}][column]')),
+            "dir": request.args.get(f'order[{i}][dir]')
+        })
+
+    # create the query
+    search_criteria = {
+        # "$or": [{ col["data"]: { "$regex": search, "$options": "i" } } for col in columns if col["searchable"] and col["data"] != None]
+        "$and": [{ col["data"]: { "$regex": col["search_value"], "$options": "i" } } for col in columns if col["searchable"] and col["data"] != None and col["search_value"] != ""]
+    }
+
+    if search_criteria["$and"] == []:
+        search_criteria = {}
+
+    # Get the total number of records
+    records_total = SAMPLES.count_documents({})
+    records_filtered = SAMPLES.count_documents(search_criteria)
+
+    # Get the data for the current page
+    data = SAMPLES.find(search_criteria).skip(offset).limit(page_size)
+    
+    # sort the data
+    for o in order:
+        data = data.sort(columns[o["column"]]["data"], pymongo.DESCENDING if o["dir"] == "desc" else pymongo.ASCENDING)
+
+    # Create the response object
+    response = {
+        'data': list(data),
+        'draw': draw,
+        'recordsFiltered' : records_filtered,
+        'recordsTotal': records_total,
+    }
+
+    return json.dumps(response, default=str)
 
 @samples.route("", methods=['POST'])
 def create_sample():
